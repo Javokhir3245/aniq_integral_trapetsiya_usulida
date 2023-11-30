a7e0cb9f299dd2e1c56ad286337c4d56c26b733e
user <abdurrahmonumar6@gmail.com>
2023-11-30 17:45:35 +0500

    Aniq integralni taqribiy hisoblash

---
+++ KursIshi/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace KursIshi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.num3 = new System.Windows.Forms.TextBox();
+            this.function = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel2.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(196, 31);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(74, 26);
+            this.num1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(318, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "b:";
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(365, 31);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(74, 26);
+            this.num2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(473, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "c:";
+            // 
+            // num3
+            // 
+            this.num3.Location = new System.Drawing.Point(520, 31);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(74, 26);
+            this.num3.TabIndex = 5;
+            // 
+            // function
+            // 
+            this.function.Location = new System.Drawing.Point(152, 86);
+            this.function.Name = "function";
+            this.function.Size = new System.Drawing.Size(442, 26);
+            this.function.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Yechish";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.yechish);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.panel1.Location = new System.Drawing.Point(12, 211);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(817, 302);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.listBox1);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.function);
+            this.panel2.Controls.Add(this.num1);
+            this.panel2.Controls.Add(this.num3);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.num2);
+            this.panel2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.panel2.Location = new System.Drawing.Point(12, 28);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(817, 177);
+            this.panel2.TabIndex = 10;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1,
+            this.toolStripButton2,
+            this.toolStripSeparator2,
+            this.toolStripButton1,
+            this.toolStripSeparator3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(841, 26);
+            this.toolStrip1.TabIndex = 11;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(177, 23);
+            this.toolStripButton1.Text = "To\'g\'ri to\'rtburchak usuli";
+            this.toolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(137, 23);
+            this.toolStripButton2.Text = "Trapetsiyalar usuli";
+            this.toolStripButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(529, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 19);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Yechim: ";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 26);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 26);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 26);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Enabled = false;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Items.AddRange(new object[] {
+            "Funksiyalar ro\'yxati",
+            "Pow(x,a)",
+            "Sqrt(x)",
+            "Log(x,a)",
+            "Sin(x)",
+            "Cos(x)",
+            "Tan(x)"});
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(143, 175);
+            this.listBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 525);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(857, 564);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(857, 564);
+            this.Name = "Form1";
+            this.Text = "Aniq integral";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox num3;
+        private System.Windows.Forms.TextBox function;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ KursIshi/Form1.cs
@@ -0,0 +1,171 @@
+﻿using NCalc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace KursIshi
+{
+    public partial class Form1 : Form
+    {
+        bool trapezia = false;
+        double a;
+        double b;
+        int n;
+        string func;
+        public Form1()
+        {
+            InitializeComponent();
+            num1.TextChanged += TextBox_TextChanged;
+            num2.TextChanged += TextBox_TextChanged;
+            num3.TextChanged += TextBox_TextChanged;
+        }
+
+        private bool T_changed()
+        {
+            string inputText = function.Text;
+            string pattern = @"\bx\b";
+            if (!Regex.IsMatch(inputText, pattern))
+            {
+                
+                return false;
+            }
+            return true;
+        }
+
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string inputText = textBox.Text;
+            string pattern = @"^\d+$";
+            if (!Regex.IsMatch(inputText, pattern))
+            {
+                MessageBox.Show("Faqat raqamlar!");
+            }
+        }
+        private void yechish(object sender, EventArgs e)
+        {
+            try
+            {
+                func = function.Text.ToString();
+                func = replace(func);
+                if (T_changed())
+                {
+                    a = Convert.ToDouble(num1.Text.ToString());
+                    b = Convert.ToDouble(num2.Text.ToString());
+                    if (a > b)
+                        throw new Exception("a b dan katta bo'lishi mumkin emas!");
+                    n = Convert.ToInt32(num3.Text.ToString());
+                    double h = (b - a) / n;
+                    List<double> x = new List<double>();
+                    List<double> y = new List<double>();
+                    Expression expr = new Expression(func);
+                    double result = 0;
+                    if (!trapezia)
+                    {
+                        do
+                        {
+                            x.Add(a);
+                            y.Add(result * h);
+                            expr.Parameters["x"] = a;
+                            a += h;
+                            result += (double)expr.Evaluate();
+
+                        } while (a < b);
+                        result *= h;
+                        CreateScatterPlot(x, y);
+                        label4.Text = "Yechim: " + (result.ToString());
+                    }
+                    else
+                    {
+
+
+                        double y0 = 0;
+                        x.Add(a);
+                        y.Add(result * h);
+                        expr.Parameters["x"] = a;
+                        y0 = (double)expr.Evaluate();
+                        result += y0;
+
+
+                        double yn = 0;
+                        x.Add(a);
+                        y.Add(result * h);
+                        expr.Parameters["x"] = b;
+                        yn = (double)expr.Evaluate();
+                        result += yn;
+
+
+
+
+                        result = (y0 + yn) / 2;
+                        for (int i = 1; i <= n - 1; i++)
+                        {
+                            x.Add(a);
+                            y.Add(result * h);
+                            expr.Parameters["x"] = a;
+                            a += h;
+                            result += (double)expr.Evaluate();
+                        }
+                        result *= h;
+                        CreateScatterPlot(x, y);
+                        label4.Text = "Yechim: " + (result.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("x qatnashmadi!!!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        private void CreateScatterPlot(List<double> x,List<double> y)
+        {
+            panel1.Controls.Clear();
+            Chart chart = new Chart();
+            chart.Size = new Size(panel1.Width, panel1.Height);
+            chart.ChartAreas.Add(new ChartArea());
+            chart.Dock = DockStyle.Fill;
+            Series series = new Series("ScatterPlot");
+            series.ChartType = SeriesChartType.Point;
+            for (int i = 0; i < x.Count; i++)
+            {
+                series.Points.AddXY(x[i], y[i]);
+            }
+            chart.Series.Add(series);
+            panel1.Controls.Add(chart);
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            trapezia = true;
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            trapezia= false;
+        }
+        private string replace(string a)
+        {
+            a=a.ToLower();
+            a = a.Replace("sin", "Sin");
+            a = a.Replace("cos", "Cos");
+            a = a.Replace("log", "Log");
+            a = a.Replace("pow", "Pow");
+            a = a.Replace("sqrt", "Sqrt");
+            a = a.Replace("tan", "Tan");
+            return a;
+        }
+    }
+}
